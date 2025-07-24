5d4eda8e0f728e2e86f019285c1555933c8c29e4
JaySeeDub <139168957+JaySeeDub@users.noreply.github.com>
2025-07-24 14:02:44 -0500

    Jet mass (#3)
    * Good Results for both, fixed the plotting error. Need to clean stuff up.
    
    * All test versions. Random Layer seems the best. Need to clean up repo.
    
    * Really successful QGAN model
    
    * Cleaned Notebooks and scripts. Everything up-to-date. Uploaded good sample checkpoints.

---
+++ Scripts/ClassGAN.py
@@ -0,0 +1,519 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# All imports
+from Imports import *
+from Helper import *
+from Preprocessing import *
+from Plotting import *
+
+# In[2]:
+
+
+# Load dataset and device
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+jet_images_path = '../data/jet-images_Mass60-100_pT250-300_R1.25_Pix25.hdf5'
+jet_mass_data = HDF5File(jet_images_path, 'r')
+
+print(jet_mass_data.keys())
+print(jet_mass_data['image'].shape)
+
+
+# In[3]:
+
+
+# Generator with Quantum Layer
+class Generator(nn.Module):
+    def __init__(self, latent_dim=256):
+        super().__init__()
+        self.latent_dim = latent_dim
+
+        self.noise = GaussianNoise(sigma=0.1)
+
+        self.feature_gen = nn.Sequential(
+            nn.Linear(9, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 256),
+            nn.LayerNorm(256),
+            self.noise
+        )
+
+        self.image_gen1 = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),  # 1x1 → 2x2
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Dropout(0.2),
+        )
+        self.image_gen2 = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),   # 2x2 → 4x4
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Dropout(0.2),
+        )
+        self.image_gen3 = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),     # 4x4 → 8x8
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.Dropout(0.2),
+        )
+
+        self.image_gen4 = nn.Sequential(
+            nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1),     # 8x8 → 16x16
+            nn.Sigmoid()
+        )
+
+    def forward(self, z_feat):
+        img = self.feature_gen(z_feat)
+        # print(img.shape)
+        img = img.view(-1, 256, 1, 1)
+        # print(img.shape)
+        img = self.image_gen1(img)
+        # print(img.shape)
+        img = self.image_gen2(img)
+        # print(img.shape)
+        img = self.image_gen3(img)
+        # print(img.shape)
+        img = self.image_gen4(img)
+        # print(img.shape)
+        img = soft_threshold(img, threshold=0.001, sharpness=1000.0)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        # Flattened image: 16x16 = 256
+        self.image_encoder = nn.Sequential(
+            spectral_norm(nn.Conv2d(1, 64, 4, 2, 1, bias=False)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.2),
+            spectral_norm(nn.Conv2d(64, 128, 4, 2, 1, bias=False)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.2),
+            spectral_norm(nn.Conv2d(128, 256, 4, 2, 1, bias=False)),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.2),
+            nn.Flatten()
+        )
+
+        self.feature_encoder = nn.Sequential(
+            nn.Linear(4, 64),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.2),
+            nn.Linear(64, 128),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.2),
+        )
+
+        # Dynamically get input dimensions
+        with torch.no_grad():
+            dummy = torch.zeros(1, 1, 16, 16)
+            flat_dim = self.image_encoder(dummy).shape[1] + 128
+
+        # Combined classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(flat_dim, 256),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.2),
+            nn.Linear(256, 64),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.2),
+            nn.Linear(64, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, img, features):
+        img_encoded = self.image_encoder(img)
+        feat_encoded = self.feature_encoder(features)
+        combined = torch.cat((img_encoded, feat_encoded), dim=1)
+
+        # print(img_encoded.shape)
+        # print(feat_encoded.shape)
+        # print(combined.shape[-1])
+
+        prob = self.classifier(combined)
+
+        return prob  # Shape: (batch_size, 1)
+
+
+# In[4]:
+
+
+# Define variable and dataset
+batch_size = 128
+n_events = int(1 * jet_mass_data['image'].shape[0])
+
+latent_dim = 256
+lr = 1e-3
+n_epochs = 100
+num = 4
+
+dataset = JetDataset(jet_mass_data, n_events)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+
+kdes = feature_distributions(dataset)
+
+print("Number of samples:", len(dataset))
+print("Image shape:", dataset.images.shape)
+print("Feature shape:", dataset.features.shape)
+
+
+# In[5]:
+
+
+# Initialize models
+generator = Generator(latent_dim).to(device)
+discriminator = Discriminator().to(device)
+
+optimizer_D = optim.Adam(discriminator.parameters(), lr=lr, betas=(0.9, 0.999))
+optimizer_G = optim.Adam(generator.parameters(), lr=lr, betas=(0.9, 0.999))
+
+scheduler_G = CosineAnnealingLR(optimizer_G, T_max=n_epochs, eta_min=1e-4)
+scheduler_D = CosineAnnealingLR(optimizer_D, T_max=n_epochs, eta_min=1e-4)
+
+g_losses = []
+d_losses = []
+
+# Tracking buffers
+stats_dict = {
+    'fake_dR_mean': [],
+    'fake_dR_std': [],
+    'fake_pixel_mean': [],
+    'fake_pixel_std': [],
+    'real_dR_mean': [],
+    'real_dR_std': [],
+    'real_pixel_mean': [],
+    'real_pixel_std': []
+}
+
+dists = compute_distance_map(16,16).to(device)
+
+
+# In[6]:
+
+
+# Plot visual of generator
+graph =  draw_graph(
+    generator,
+    input_size=(1, 9),
+    expand_nested=True,
+    depth = 1,
+    graph_name="Discriminator Architecture",
+    mode = 'train',
+    show_shapes = True,
+    hide_inner_tensors=True,   # Hides intermediate shapes
+    roll=True,                  # More compact layer grouping
+    save_graph=False,           # Set to True if you want to save
+    # node_attr={"color": "lightblue", "style": "filled"},  # Node styling
+    # edge_attr={"color": "gray"},                          # Edge styling
+    graph_dir= "TB",                         # Layout: TB (top-bottom), LR (left-right)
+)
+
+# Show in notebook
+graph.visual_graph
+
+
+# In[7]:
+
+
+# Plot visual of discriminator
+graph2 = draw_graph(
+    discriminator,
+    input_size=[(1, 1, 16, 16),(1,4)],
+    expand_nested=True,
+    depth = 1,
+    graph_name="Discriminator Architecture",
+    mode = 'train',
+    show_shapes = True,
+    hide_inner_tensors=True,   # Hides intermediate shapes
+    roll=True,                  # More compact layer grouping
+    save_graph=False,           # Set to True if you want to save
+    # node_attr={"color": "lightblue", "style": "filled"},  # Node styling
+    # edge_attr={"color": "gray"},                          # Edge styling
+    graph_dir= "TB",                         # Layout: TB (top-bottom), LR (left-right)
+)
+graph2.visual_graph
+
+
+# In[15]:
+
+
+## Load a previous model
+load = True
+
+if load:
+    # load_path = "models/class_gan_model_0722_2207.pt" # Path to load your model
+    load_path = "models/class_gan_model_m21_1003.pt"  # Sample model
+    
+    # Load the checkpoint
+    checkpoint = torch.load(load_path)
+    
+    # Restore model weights
+    generator.load_state_dict(checkpoint["generator_state_dict"])
+    discriminator.load_state_dict(checkpoint["discriminator_state_dict"])
+    
+    # Optionally restore tracking data
+    g_losses = checkpoint["g_losses"]
+    d_losses = checkpoint["d_losses"]
+    
+    stats_dict = checkpoint["stats_dict"]
+    
+    print(f"Loaded model from {load_path}")
+
+
+# In[9]:
+
+
+# Training loop
+if True:
+    for epoch in range(n_epochs):
+        for i, (real_image, real_features, flipped_image, flipped_features) in enumerate(dataloader):
+            
+            # All real data are normalized in the dataloader
+            real_feat = real_features.to(device)
+            real_flipped_feat = flipped_features.to(device)
+            real_img = real_image.unsqueeze(1).to(device)
+            real_flipped_img = flipped_image.unsqueeze(1).to(device)
+    
+            # print(f"Real: {real_img.shape}")
+    
+            # Codings will be label, eta, pT, mass that get passed directly to the discriminator
+            # All feature values get passed to the generator, then the output image passed to the discriminator
+    
+            # Discriminator training
+            if i % 3 == 0:
+                optimizer_D.zero_grad()
+                discriminator.train()
+                # Generate fake samples
+                # Should be very easy to modify which values are passed as codings
+                z_codings = torch.cat([torch.randint(0, 2, (batch_size, 1)), 
+                                      sample_fit_noise(kdes, num_samples=batch_size)[:,:]],
+                                      dim=1).to(device)
+
+                # Generate the fake image
+                fake_img = generator(z_codings)
+                # print(f"Fake: {fake_img.shape}")
+    
+                # Generate eta-flipped data
+                flipped_z_codings = z_codings.clone()
+                flipped_z_codings[:, 1] *= -1
+
+                fake_flipped_img = generator(flipped_z_codings)
+    
+                # Get predictions and labels
+                real_disc_codings = real_feat[:,:num]
+                real_flipped_disc_codings = real_flipped_feat[:,:num]
+                fake_disc_codings = z_codings[:,:num]
+                fake_flipped_disc_codings = flipped_z_codings[:,:num]
+
+                # Discriminator gets codings + image
+                real_pred = discriminator(real_img, real_disc_codings)
+                real_flipped_pred = discriminator(real_flipped_img, real_flipped_disc_codings)
+                fake_pred = discriminator(fake_img, fake_disc_codings)
+                fake_flipped_pred = discriminator(fake_flipped_img, fake_flipped_disc_codings)
+    
+                preds = (torch.cat([real_pred, real_flipped_pred, fake_pred, fake_flipped_pred], dim=0)).squeeze(1)
+
+                # Labels
+                ones = torch.ones(2*len(fake_pred))
+                zeros = torch.zeros(2*len(real_pred))
+                labels = (torch.cat([ones, zeros], dim=0)).to(device)
+    
+                # Discriminator loss is just its ability to distinguish
+                d_loss = torch.nn.BCELoss()(preds, labels)
+    
+                d_loss.backward()
+                optimizer_D.step()
+    
+            # Generator Training
+            if i % 1 == 0:
+                optimizer_G.zero_grad()
+                generator.train()
+                
+                # Generate fake data
+                # Should be very easy to modify which values are passed as codings
+                z_codings = torch.cat([torch.randint(0, 2, (batch_size, 1)), 
+                                      sample_fit_noise(kdes, num_samples=batch_size)[:,:]],
+                                      dim=1).to(device)
+    
+                # Generate image
+                fake_img = generator(z_codings)
+    
+                # Generate eta-flipped data
+                flipped_z_codings = z_codings.clone()
+                flipped_z_codings[:, 1] *= -1
+
+                fake_flipped_img = generator(flipped_z_codings)
+
+                ## BCE Discriminator Loss
+                # Discriminator gets codings + generated image
+                fake_disc_codings = z_codings[:,:num]
+                fake_flipped_disc_codings = flipped_z_codings[:,:num]
+
+                # Get predictions and labels
+                fake_pred = discriminator(fake_img, fake_disc_codings)
+                fake_flipped_pred = discriminator(fake_flipped_img, fake_flipped_disc_codings)
+
+                target = torch.ones_like(fake_pred)
+                bce = nn.BCELoss()
+                validity_loss = bce(fake_pred, target) + bce(fake_flipped_pred, target)
+    
+                ## Stat loss
+                # Compute statistics for original and flipped fake images
+                fake_stats_orig = compute_fake_statistics(fake_img.to('cpu'), dists.to('cpu'))
+                fake_stats_flip = compute_fake_statistics(fake_flipped_img.to('cpu'), dists.to('cpu'))
+                
+                # Average the statistics
+                fake_stats = {
+                    'fake_dR_mean': 0.5 * (fake_stats_orig['fake_dR_mean'] + fake_stats_flip['fake_dR_mean']),
+                    'fake_dR_std': 0.5 * (fake_stats_orig['fake_dR_std'] + fake_stats_flip['fake_dR_std']),
+                    'fake_pixel_mean': 0.5 * (fake_stats_orig['fake_pixel_mean'] + fake_stats_flip['fake_pixel_mean']),
+                    'fake_pixel_std': 0.5 * (fake_stats_orig['fake_pixel_std'] + fake_stats_flip['fake_pixel_std']),
+                }
+                
+                # Move to device
+                fake_dR_mean = fake_stats['fake_dR_mean'].to(device)
+                fake_dR_std = fake_stats['fake_dR_std'].to(device)
+                fake_pixel_mean = fake_stats['fake_pixel_mean'].to(device)
+                fake_pixel_std = fake_stats['fake_pixel_std'].to(device)
+                
+                # Get real stats from z_codings (features 5–8)
+                real_dR_mean = z_codings[:,5]
+                real_dR_std = z_codings[:,6]
+                real_pixel_mean = z_codings[:,7]
+                real_pixel_std = z_codings[:,8]
+                
+                real_stats = {
+                    'real_dR_mean': real_dR_mean,
+                    'real_dR_std': real_dR_std,
+                    'real_pixel_mean': real_pixel_mean,
+                    'real_pixel_std': real_pixel_std
+                }
+                
+                # stats_dict = track_statistics(fake_stats, real_stats)
+                # plot_tracked_statistics(stats_dict)
+                
+                if n_epochs - epoch <= 10:
+                    # Track the last statistics
+                    stats_dict = track_statistics(fake_stats, real_stats)
+                    plot_tracked_statistics(stats_dict)
+                
+                # Statistical KL Divergence loss
+                kl_total = 0
+                kl1 = kde_kl_divergence_torch(real_dR_mean / batch_size, fake_dR_mean) / 0.000001
+                # print(f"KL(real_dR_mean, fake_dR_mean) = {kl1.item()}")
+                
+                kl2 = kde_kl_divergence_torch(real_dR_std / batch_size, fake_dR_std) / 0.003
+                # print(f"KL(real_dR_std, fake_dR_std) = {kl2.item()}")
+                
+                kl3 = kde_kl_divergence_torch(real_pixel_mean / batch_size, fake_pixel_mean) / 0.00000002
+                # print(f"KL(real_pixel_mean, fake_pixel_mean) = {kl3.item()}")
+                
+                kl4 = kde_kl_divergence_torch(real_pixel_std / batch_size, fake_pixel_std) / 0.00015
+                # print(f"KL(real_pixel_std, fake_pixel_std) = {kl4.item()}")
+                
+                kl_total = kl1 + kl2 + kl3 + kl4
+    
+                stat_loss = kl_total
+                
+                ## Number non-zero loss
+                fake_nnz = soft_count_nonzero(fake_img, threshold=3e-3, sharpness=10000.0)
+                real_nnz = soft_count_nonzero(real_img, threshold=3e-3, sharpness=10000.0)
+                nnz_loss = torch.nn.MSELoss()(fake_nnz, real_nnz)
+    
+                
+                # Total generator loss is the average of the discriminator's predictions of the original and flipped data
+                # + number of non-zero pixels loss
+                # + the difference between input and output dR and pixel statistics
+    
+                alpha = .4
+                beta = .001
+                chi = .02
+    
+                g_loss = (alpha*validity_loss + beta*nnz_loss + chi*stat_loss)
+
+                g_loss.backward()
+                optimizer_G.step()
+    
+        g_losses.append(g_loss.item())
+        d_losses.append(d_loss.item())
+
+        print(f"[Epoch {epoch+1}/{n_epochs}] [D loss: {d_losses[-1]:.4f}] [G loss: {g_losses[-1]:.4f}] [Validity_loss: {alpha*validity_loss:.4f}] \n [Stat_loss: {chi*stat_loss:.4f}] [NNZ_loss: {beta*nnz_loss:.4f}]") 
+
+        plot_generated_samples(generator, dataset, kdes, batch_size=16, latent_dim=256)
+
+        if epoch % 10 == 0:
+            # Save model states and tracked data in a temp file during training
+            save_path = f"models/class_gan_model_temp.pt"
+            torch.save({
+                "generator_state_dict": generator.state_dict(),
+                "discriminator_state_dict": discriminator.state_dict(),
+                "g_losses": g_losses,
+                "d_losses": d_losses,
+                "stats_dict": stats_dict
+            }, save_path)
+
+            print(f"Model and statistics saved to {save_path}")
+
+
+# In[10]:
+
+
+## Save Model
+# Create output directory if it doesn't exist
+
+save = True
+
+if save:
+    os.makedirs("models", exist_ok=True)
+
+    # Timestamp for unique filenames
+    timestamp = datetime.now().strftime("%m%d_%H%M")
+    
+    # Save model states and tracked data in a single file
+    save_path = f"models/class_gan_model_{timestamp}.pt"
+    torch.save({
+        "generator_state_dict": generator.state_dict(),
+        "discriminator_state_dict": discriminator.state_dict(),
+        "g_losses": g_losses,
+        "d_losses": d_losses,
+        "stats_dict": stats_dict
+    }, save_path)
+    
+    print(f"Model and statistics saved to {save_path}")
+
+
+# In[ ]:
+
+
+plot_real_samples(dataset)
+
+
+# In[16]:
+
+
+test_generated_samples(generator, discriminator, dataset, kdes, batch_size=100000)
+
+# Optional args:
+#     generator,
+#     discriminator,
+#     dataset,
+#     kdes,
+#     batch_size=16,
+#     latent_dim=256,
+#     codings=None,
+#     plot_distributions=True,
+#     compare_discriminator=True
+
+plot_metrics(g_losses, d_losses)
+
+
+# In[ ]:
+
+
+
+
